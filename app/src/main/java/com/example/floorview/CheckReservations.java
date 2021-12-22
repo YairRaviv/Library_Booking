3615d7bbf04101b8a04a825fcddc9680f6162ee3
yair raviv <yairaviv83@gmail.com>
2021-12-22 22:12:29 +0200

    created ClassRoomRequest Activity , made few changes in login/registration screen

---
+++ app/src/main/java/com/example/floorview/CheckReservations.java
@@ -0,0 +1,14 @@
+package com.example.floorview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckReservations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_reservations);
+    }
+}
---
+++ app/src/main/java/com/example/floorview/LibrarianMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.floorview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LibrarianMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_librarian_main);
+    }
+}
